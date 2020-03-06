aef4b256871113ff391a6d3243a815c776d6eca8
Mohsen Asghari <mohsen.asghari@louisville.edu>
2020-03-06 01:07:04 -0500

    1.0.0

---
+++ DataCollections/Twitter/Config.py
@@ -0,0 +1,10 @@
+class Twitter():
+    Consumer_Key = ''
+    Consumer_Secret = ''
+    Access_Token = ''
+    Access_Token_Secret = ''
+    CONNECTION_STRING = "sqlite:///Twitter.db"
+    TABLE_NAME = "tweets"
+    CSV_NAME = "tweets.csv"
+    AVOID_DICTIONARY = ""#"job|Job|(hir\w+)|career|hiring"
+    LANG = ["en"]#,"fa","ar"]
